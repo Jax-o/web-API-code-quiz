ce6d666025ce87739132f43e794a6a1b6939cf14
Jacklyn Malone <jjmlmhc.09@gmail.com>
2021-09-21 16:25:17 -0400

    added new files

--- highscores.js
+++ highscores.js
@@ -1,29 +1,15 @@
+var backButton = document.querySelector("#back");
+var clearButton = document.querySelector("#clear");
 
-var questionDiv = document.querySelector('#quizQuestions');
+var score = localStorage.getItem("score");
 
-var answerDiv = document.querySelector('#quizAnswers');
+backButton.addEventListener("click", function() {
 
-quizAnswers.addEventListener("click", pickAnswer);
-
-// change questions to JS fundamentals
-var quizChoices = [
-	{
-		question: 'Inside which HTML element do you put the JavaScript',
-		selection: ['scripting', 'script', 'javascript', 'js'],
-	},
-	{
-		question: 'How do you call a function named myFunction',
-		selection: ['myFunction()', 'call function myFunction()', 'call myFunction()', 'call Function'],
-	},
-	{
-		question: 'What does HTML stand for',
-		selection: ['Hperlinks and Text Markup Language', 'Hyper Text Markup Language', 'Home Tool Markup Language', 'Hyper Text Model Language'],
-	},
-	{
-		question: 'What does CSS stand for',
-		selection: ['Computer Style Sheets', 'Cascading Style Sheets', 'Creative Style Sheets', 'Colorful Style Sheets'],
-	},
-
-];
-currentQuestion = 0;
-correctAnswers = 0;
+	localStorage.setItem("score", score);
+	}
+ });
+ 
+ clearButton.addEventListener("click", function() {
+	localStorage.setItem("score", score);
+ });
+ 
--- main.js
+++ main.js
@@ -1,48 +1,85 @@
+// declare variables
 var startButton = document.querySelector("#start-button");
 var mainContent = document.querySelector("#content");
-var choices = document.querySelector("choices");
-
-var listEl = document.createElement('ol');
-
-var li1 = document.createElement("li");
-var li2 = document.createElement("li");
-var li3 = document.createElement("li");
-var li4 = document.createElement("li");
-
-li1.textContent = 'scripting'
-li2.textContent = 'script'
-li3.textContent = 'javascript'
-li4.textContent = 'js'
+var timerEl = document.getElementById('countdown');
+var choices = document.querySelector('#choices')
+var selectQuestion = 0;
+var selectAnswer = 0
 
 
+
+
+// declare quiz array
 var quizQuestions = [
 	{
 		questionTitle: 'Inside which HTML element do you put the JavaScript?',
 		answers: ['scripting', 'script', 'javascript', 'js'],
+		answer: 'script',
 	},
-	
 
-	{	
+
+	{
 		questionTitle: 'How do you call a function named myFunction',
 		answers: ['myFunction()', 'call function myFunction()', 'call myFunction()', 'call Function'],
+		answer: 'myFunction()',
+
 	},
-	
+
 	{
 		questionTitle: 'What does HTML stand for',
 		answers: ['Hperlinks and Text Markup Language', 'Hyper Text Markup Language', 'Home Tool Markup Language', 'Hyper Text Model Language'],
+		answer: 'Hyper Text Markup Language',
 	},
-	
+
 	{
 		questionTitle: 'What does CSS stand for',
 		answers: ['Computer Style Sheets', 'Cascading Style Sheets', 'Creative Style Sheets', 'Colorful Style Sheets'],
+		answer: 'Cascading Style Sheets',
 	},
 ]
 
-startButton.addEventListener("click", function() {
-mainContent.innerHTML = (quizQuestions[0].questionTitle + 
-	listEl.appendChild(li1));
-	listEl.appendChild(li2);
-	listEl.appendChild(li3);
-	listEl.appendChild(li4);
 
-});
+
+// event listeners
+
+startButton.addEventListener("click", function () {
+	startGame()
+	} 
+);
+
+// answerChoices.addEventListener('click', function () {
+// 	answerVerify()
+// }
+
+// functions 
+function startGame() {
+	mainContent.style.display = 'block';
+	function countdown() {
+		var timeLeft = 10;
+	
+		var timeInterval = setInterval(function () {
+			if (timeLeft > 1) {
+				 timerEl.textContent = 'Time: ' + timeLeft;
+				timeLeft--;
+			} else {
+				timerEl.textContent = '';
+				clearInterval(timeInterval);
+			}
+		}, 1000);
+	}
+	
+	countdown();
+	quizSelection();
+}
+
+function quizSelection() {
+	mainContent.style.display = 'block';
+	mainContent.innerHTML = quizQuestions[selectQuestion].questionTitle + quizQuestions[selectAnswer].answers
+
+	
+}
+
+// function for quizQuestions
+// for loop for list items, add event listeners checking the right answer function to answers choices 
+// display the question
+// function for checking the right answer
